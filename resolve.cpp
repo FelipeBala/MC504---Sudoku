e9fb994843dbcb8abb36f36215f2bab53add7b66
FelipeBala <felipebalabanian@gmail.com>
2014-03-16 22:53:18 -0300

    Create resolve.cpp

---
+++ resolve.cpp
@@ -0,0 +1,13 @@
+
+#include "sudoku.h";
+int main( int argc, char *argv[] ) {
+
+Sudoku teste;
+teste.inicia_dados_arquivo_x(argv[1]);
+teste.fornece_dicas();
+teste.rearranja();
+teste.resolve();
+teste.imprime_dicas();
+return(0);
+}
+
